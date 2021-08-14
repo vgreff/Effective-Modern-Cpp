f488341444e46eab4e9f8c8af851782ace93f181
vincent.greff <vince@greffsite.com>
2021-08-14 11:13:40 -0500

    more boost types

--- Item04_Know_how_to_view_deduced_types/boost_type_indexVG.cpp
+++ Item04_Know_how_to_view_deduced_types/boost_type_indexVG.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <typeinfo>
 #include <vector>
+#include <string>
 
 #include <boost/type_index.hpp>
   using std::cout;
@@ -21,7 +22,7 @@ void f(const T& param)
   // show T
   cout << "T =     "
        << type_id_with_cvr<T>().pretty_name()
-       << '\n';
+       << ' ';
 
   // show param's type
   cout << "param = "
@@ -37,24 +38,74 @@ std::vector<Widget> createVec()    // factory function
     return vw;
 }
 
+template<typename T>
+void pur(std::string name,  T&& param)
+{
+  cout << name << " (PARM)"
+       << " | T = "
+       << type_id_with_cvr<T>().pretty_name()
+       << ' ';
+  cout << " | param = "
+       << type_id_with_cvr<decltype(param)>().pretty_name()
+       << '\n';
+
+  cout << name << " (FORW)"
+       << " | T = "
+       << type_id_with_cvr<T>().pretty_name()
+       << ' ';
+  cout << " | param = "
+       << type_id_with_cvr<decltype(std::forward<T>(param))>().pretty_name()
+       << '\n';
+
+//local variables
+auto  val = param;
+  cout << name << " (AUTO)"
+       << " | T = "
+       << type_id_with_cvr<T>().pretty_name()
+       << ' ';
+  cout << " | param = "
+       << type_id_with_cvr<decltype(val)>().pretty_name()
+       << '\n';
+
+auto decl2(std::forward<T>(param));
+  cout << name << " (AFOR)"
+       << " | T = "
+       << type_id_with_cvr<T>().pretty_name()
+       << ' ';
+  cout << " | param = "
+       << type_id_with_cvr<decltype(decl2)>().pretty_name()
+       << '\n';
+
+T&& decl1(std::forward<T>(param));
+  cout << name << " (UREF)"
+       << " | T = "
+       << type_id_with_cvr<T>().pretty_name()
+       << ' ';
+  cout << " | param = "
+       << type_id_with_cvr<decltype(decl1)>().pretty_name()
+       << '\n'
+       << '\n';
+}
+
+
 int main()
 {
 
-    const auto vw0 = createVec();        // init vw w/factory return
     auto vw = createVec();        // init vw w/factory return
+    const auto vw0 = createVec();        // init vw w/factory return
     auto& vw1(vw); 
     auto&& vw2(createVec()); 
 
+    cout << "param  = "
+       << type_id_with_cvr<decltype(vw)>().pretty_name()
+       << '\n';
     cout << "param0 = "
        << type_id_with_cvr<decltype(vw0)>().pretty_name()
        << '\n';
     cout << "param1 = "
-       << type_id_with_cvr<decltype(vw)>().pretty_name()
-       << '\n';
-    cout << "param2 = "
        << type_id_with_cvr<decltype(vw1)>().pretty_name()
        << '\n';
-    cout << "param3 = "
+    cout << "param2 = "
          << type_id_with_cvr<decltype(vw2)>().pretty_name()
          << " | " << type_id_with_cvr<decltype(vw2)>().name()
          << '\n';
@@ -63,4 +114,9 @@ int main()
       f(&vw[0]);                        // call f
       // ...
     }
+    pur("RV val", std::vector<Widget>() );
+    pur("RV ptr", new std::vector<Widget>() );
+    pur("LV val", vw );
+    pur("LV cva", vw0 );
+    // pur("RV", std::vector<Widget>() );
 }
